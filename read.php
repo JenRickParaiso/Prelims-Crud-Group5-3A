1cadcc2ce5d25ccbbb85b511ce9722f7b7e4c98a
Jen Rick Paraiso <123245424+JenRickParaiso@users.noreply.github.com>
2023-02-09 20:57:06 +0800

    Update read.php
    Groupmates are cooperating well

--- read.php
+++ read.php
@@ -25,8 +25,10 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                 
                 // Retrieve individual field value
                 $name = $row["name"];
-                $address = $row["address"];
-                $salary = $row["salary"];
+				$age = $row["age"];
+				$contactnumber = $row["contactnumber"];
+                $emailaddress = $row["emailaddress"];
+                $gender = $row["gender"];
             } else{
                 // URL doesn't contain valid id parameter. Redirect to error page
                 header("location: error.php");
@@ -73,13 +75,21 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                         <label>Name</label>
                         <p><b><?php echo $row["name"]; ?></b></p>
                     </div>
-                    <div class="form-group">
-                        <label>Address</label>
-                        <p><b><?php echo $row["address"]; ?></b></p>
+					 <div class="form-group">
+                        <label>Age</label>
+                        <p><b><?php echo $row["age"]; ?></b></p>
                     </div>
                     <div class="form-group">
-                        <label>Salary</label>
-                        <p><b><?php echo $row["salary"]; ?></b></p>
+					 <div class="form-group">
+                        <label>Contact #</label>
+                        <p><b><?php echo $row["contactnumber"]; ?></b></p>
+                    </div>
+                        <label>Email Address</label>
+                        <p><b><?php echo $row["emailaddress"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Gender</label>
+                        <p><b><?php echo $row["gender"]; ?></b></p>
                     </div>
                     <p><a href="index.php" class="btn btn-primary">Back</a></p>
                 </div>
